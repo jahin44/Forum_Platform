b52c8af4598331e21edf07dff29a76ba28068f9c
jahin44 <jahinhasan1@gmail.com>
2022-01-16 21:04:20 +0600

    CreateBoard Board list

---
+++ Forum/Forum.Web/Models/Moderator/BoardListModel.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Forum.System.BusinessObjects;
+using Forum.System.Services;
+
+namespace Forum.Web.Models.Moderator
+{
+    public class BoardListModel
+    {
+        public IList<Board> Boards { get; set; }
+        private ILifetimeScope _scope;
+        private IBoardService _boardService;
+        public BoardListModel() { }
+
+        public BoardListModel(IBoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        public void Resolve(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _boardService = _scope.Resolve<IBoardService>();
+        }
+
+        public void LoadAllBoards()
+        {
+            Boards = _boardService.GetAllBoards();
+        }
+    }
+}
